173f53580395e8e06a51113a5173fe1ef3f400d0
Graknar <graknarlorque@gmail.com>
2021-08-15 16:11:11 +0200

    create home menu
    création du menu home

---
+++ src/main/java/Menus/Home.java
@@ -0,0 +1,18 @@
+package Menus;
+
+import Main.Input;
+import java.awt.*;
+import javax.swing.*;
+
+public class Home implements Menu{
+
+    @Override
+    public Menu Update(Input _inputs) {
+        return null;
+    }
+
+    @Override
+    public void draw(Graphics _graphics) {
+
+    }
+}
