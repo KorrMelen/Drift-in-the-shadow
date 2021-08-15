ccc1870a0fc6590aeaa9d0e8b3499d8d460c691b
Nathan Touroux <touroux.nathan@gmail.com>
2021-08-15 14:54:59 +0200

    add JFrame

---
+++ src/main/java/Frame.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+class Frame extends JPanel implements KeyListener{
+    JFrame frame = new JFrame();
+    static int width = 1280;
+    static int height = 720;
+
+    Frame() {
+        this.setPreferredSize(new Dimension(width,height));
+        frame.setResizable(true);
+        frame.setContentPane(this);
+        frame.pack();
+        frame.setVisible(true);
+        frame.addKeyListener(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(0,0,this.getWidth(),this.getHeight());
+    }
+    
+    
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case 90 : System.out.println("A"); break;
+        }
+    }
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
