ee47b41d8e66498bef03c7fede35f18fe22d0918
Graknar <graknarlorque@gmail.com>
2021-08-15 18:31:48 +0200

    ajout tag dans les boutons

--- src/main/java/Menus/Home.java
+++ src/main/java/Menus/Home.java
@@ -11,11 +11,21 @@ import javax.swing.*;
 public class Home implements Menu{
 
     private Button[] buttons = new Button[]{
-            new Button(Setting.width/2-125, Setting.height/4-30,250,30,"Let's play a game")
+            new Button("Play",Setting.width/2-125, Setting.height/4-30,250,30,"Let's play a game")
     };
 
     @Override
     public Menu update(Input _inputs) {
+        try {
+            for (Point pos : _inputs.clicksPositions()) {
+                for (Button button : this.buttons) {
+                    if (button.isClicked(pos.x, pos.y))
+                        System.out.print(button.tag);
+                }
+            }
+        }catch (Exception ex){
+
+        }
         return this;
     }
 
